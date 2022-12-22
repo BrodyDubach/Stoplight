0c6ed3faa2e6f0341403c3ef210f8c431de87880
Valerieclaire96 <Valerieclaire96@gmail.com>
2022-12-22 01:05:19 +0000

    stop light before extra credit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [home, setHome] = useState("red");
+	
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="stick mx-auto"></div>
+		<div className="container">
+			<div  className={home === 'red' ? 'red light glow': 'red light'} onClick={()=> setHome("red")}></div>
+			<div className={home === 'yellow'? 'yellow light glow' : 'yellow light'} onClick={()=> setHome("yellow")}></div>
+			<div className={home === 'green' ? 'green light glow' : 'green light'} onClick={()=> setHome("green")}></div>
+			{/* <div  className={"light red" + (home === 'red' ? 'glow':"")} onClick={()=> setHome("red")}></div>
+			<div className={"light yellow" + (home === 'yellow'?  'glow':"")} onClick={()=> setHome("yellow")}></div>
+			<div className={"light green" + (home === 'green' ? 'glow': "")} onClick={()=> setHome("green")}></div> */}
 		</div>
-	);
+		</>
+	)
 };
 
 export default Home;
