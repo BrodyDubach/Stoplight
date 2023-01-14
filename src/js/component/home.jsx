c507f13375c426a1a06ce9bc73038c33c0bd570c
BrodyDubach <brodydubach@gmail.com>
2023-01-14 22:37:39 +0000

    Changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
 	let [home, setHome] = useState("red");
 	
@@ -15,9 +12,7 @@ const Home = () => {
 			<div  className={home === 'red' ? 'red light glow': 'red light'} onClick={()=> setHome("red")}></div>
 			<div className={home === 'yellow'? 'yellow light glow' : 'yellow light'} onClick={()=> setHome("yellow")}></div>
 			<div className={home === 'green' ? 'green light glow' : 'green light'} onClick={()=> setHome("green")}></div>
-			{/* <div  className={"light red" + (home === 'red' ? 'glow':"")} onClick={()=> setHome("red")}></div>
-			<div className={"light yellow" + (home === 'yellow'?  'glow':"")} onClick={()=> setHome("yellow")}></div>
-			<div className={"light green" + (home === 'green' ? 'glow': "")} onClick={()=> setHome("green")}></div> */}
+
 		</div>
 		</>
 	)
